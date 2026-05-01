14de35fc9b744573a684634465246ac10218679f
Sagar Patra <160384227+sagarpatra07@users.noreply.github.com>
2026-05-01 14:11:11 +0000

    Solved two sum problem using 2 pointers

---
+++ Arrays/TwoPointers/README.md
@@ -0,0 +1,14 @@
+# Two Pointer Pattern
+
+## When to Use
+- Sorted array
+- Merge/ Remove Duplicate/ Rearrange
+- Pair finding
+- Opposite direction traversal
+
+## Common Problems
+- Two Sum (Sorted)
+- Container With Most Water
+
+## Trick
+Move pointers based on condition (increase/decrease sum)
---
+++ Arrays/TwoPointers/two_sum.cpp
@@ -0,0 +1,36 @@
+/*
+Problem: Two Sum II
+Platform: LeetCode
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+Approach:
+- Using 2 Pointers method
+- Check sum
+
+Time Complexity: 
+Space Complexity: 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int start = 0;
+        int end = numbers.size() - 1;
+
+        while(start<end){
+            int sum = numbers[start] + numbers[end];
+
+            if(sum == target){
+                return {start+1, end+1}; //As it's 1-indexed array
+            } else if(sum < target){
+                start++;
+            } else { 
+                end--; //if sum > target
+            }
+        }
+        return {};
+    }
+};
